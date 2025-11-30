bc828dde9a0aa851cab81e8c91101631df99c261
CjBallesil <cjballesil@att.net>
2025-11-29 23:21:17 -0600

    Raycast hit on objects when pressing E

--- Assets/Scripts/PlayerPickUpDrop.cs
+++ Assets/Scripts/PlayerPickUpDrop.cs
@@ -2,25 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerPickUpDrop : MonoBehaviour
 {
     [SerializeField] private Transform playerCameraTransform;
     [SerializeField] private LayerMask pickUpLayerMask;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            float pickupDistance = 2f;
-            Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickupDistance, pickUpLayerMask);
+            float pickUpDistance = 5f;
+            if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask))
+            {
+                Debug.Log("Raycast hit " + raycastHit.transform.name);
+            }
         }
-        
     }
 }
